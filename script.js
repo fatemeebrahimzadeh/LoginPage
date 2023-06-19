8943de7e64f670fa7101b0a28d572eb324a5e05a
fateme ebrahimzadeh <fateme.ebrahimzadeh20@gmail.com>
2023-06-19 14:04:00 +0330

    complete script

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
-// TODO:
+const signupPopup = document.querySelector(".signup-popup");
+const loginPopup = document.querySelector(".login-popup");
+
+const loginButton = document.querySelector(".login-button");
+const signupButton = document.querySelector(".signup-button");
+
+const buttonHanler = (event, panelType) => {
+  event.preventDefault();
+  loginPopup.classList.toggle("hidden");
+  signupPopup.classList.toggle("hidden");
+};
+
+signupButton.addEventListener("click", (event) =>
+  buttonHanler(event, "signup")
+);
+loginButton.addEventListener("click", (event) => buttonHanler(event, "login"));
+
+// axios support
+function onSubmitHanler(event) {
+  event.preventDefault();
+  let data = getFormData(event.target);
+  console.log(data);
+  //  validation check
+  // data is prepared for sending to the server
+}
+
+function getFormData(form) {
+  const formData = new FormData(form);
+  const data = {};
+  formData.forEach((value, key) => {
+    data[key] = value;
+  });
+  if (Object.keys(data).length === 0) {
+    console.warn("No form data found");
+  }
+  return data;
+}
